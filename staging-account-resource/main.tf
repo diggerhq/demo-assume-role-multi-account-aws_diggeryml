4500de07ab66daaddef07ac3885b2cffef91abfb
Mohamed Habib <moe.habib9@gmail.com>
2023-09-12 14:08:47 +0000

    fix tform

--- staging-account-resource/main.tf
+++ staging-account-resource/main.tf
@@ -1,6 +1,14 @@
 
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
 
 resource "aws_instance" "web" {
   ami           = "ami-0b0ea68c435eb488d"
   instance_type = "t3.micro"
+  vpc_id = data.aws_vpc.id
+
+  tags = {
+    Name = "staging acc ec2 instance"
+  }
 }
--- test-account-resource/main.tf
+++ test-account-resource/main.tf
@@ -1,6 +1,14 @@
 
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
 
 resource "aws_instance" "web" {
   ami           = "ami-0b0ea68c435eb488d"
   instance_type = "t3.micro"
+  vpc_id = data.aws_vpc.id
+
+  tags = {
+    Name = "test acc ec2 instance"
+  }
 }
