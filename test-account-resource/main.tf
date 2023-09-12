642bf345b397a6985120e82d8e9b46793601261a
Mohamed Habib <moe.habib9@gmail.com>
2023-09-12 15:04:52 +0000

    update bucket

--- test-account-resource/main.tf
+++ test-account-resource/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0b0ea68c435eb488d"
   instance_type = "t3.micro"
-  subnet_id = "subnet-03b00a2de9b75f292"
+  subnet_id = "subnet-05d1193996f3d5cc1"
 
   tags = {
     Name = "test acc ec2 instance"
