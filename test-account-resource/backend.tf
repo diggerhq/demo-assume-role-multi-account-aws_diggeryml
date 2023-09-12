6fcd66a4e093d402e404b042c5bb68eed81da631
Mohamed Habib <moe.habib9@gmail.com>
2023-09-12 10:41:19 +0000

    init for test account

---
+++ test-account-resource/backend.tf
@@ -0,0 +1,8 @@
+
+terraform {
+  backend "s3" {
+    bucket = "digger-tests-terraform-states"
+    key    = "oidcs-multi-account/test"
+    region = "us-east-2"
+  }
+}
---
+++ test-account-resource/main.tf
@@ -0,0 +1,6 @@
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0b0ea68c435eb488d"
+  instance_type = "t3.micro"
+}
