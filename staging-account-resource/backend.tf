3efa163dfc32660f569489c871d608c5f321f060
Mohamed Habib <moe.habib9@gmail.com>
2023-09-12 14:00:33 +0000

    add staging account

---
+++ staging-account-resource/backend.tf
@@ -0,0 +1,12 @@
+
+terraform {
+  backend "s3" {
+    bucket = "digger-tests-terraform-states"
+    key    = "oidcs-multi-account/test"
+    region = "us-east-2"
+  }
+}
+
+provider "aws" {
+  profile = "stagingaccount"
+}
