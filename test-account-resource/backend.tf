692cdc8e524b338ccf5a7e57225136ffe14b38fb
Mohamed Habib <moe.habib9@gmail.com>
2023-09-12 16:25:08 +0000

    fix test bucket

--- test-account-resource/backend.tf
+++ test-account-resource/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket = "dias-test-terraform-state"
     key    = "oidcs-multi-account/test"
-    region = "us-east-2"
+    region = "us-east-1"
     profile = "testaccount"
   }
 }
