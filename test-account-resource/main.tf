f4c5b0e5780a9df19dceefdb0db9ff4c6b17dbbe
Mohamed Habib <moe.habib9@gmail.com>
2023-09-12 14:14:09 +0000

    default vpc

--- test-account-resource/main.tf
+++ test-account-resource/main.tf
@@ -1,12 +1,12 @@
 
 data "aws_vpc" "selected" {
-  id = var.vpc_id
+  default = true
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-0b0ea68c435eb488d"
   instance_type = "t3.micro"
-  vpc_id = data.aws_vpc.id
+  vpc_id = data.aws_vpc.selected.id
 
   tags = {
     Name = "test acc ec2 instance"
