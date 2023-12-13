b44aa95c67407f1034fff40d777d3a8fc0bbf94d
Mohamed Habib <moe.habib9@gmail.com>
2023-12-13 22:35:05 +0000

    Update backend.tf

--- staging-account-resource/backend.tf
+++ staging-account-resource/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket = "digger-tests-terraform-states"
     key    = "oidcs-multi-account-diggeryml/staging"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
 
